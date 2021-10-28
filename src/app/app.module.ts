bbb626df40f37b51dd2375daca9f823813442d30
alik321 <aliar.ulanov@gmail.com>
2021-10-29 03:33:56 +0600

    Create a project appStore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StorePageComponent } from './store-page/store-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StorePageComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/store-page/store-page.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-store-page',
+  templateUrl: './store-page.component.html',
+  styleUrls: ['./store-page.component.css']
+})
+export class StorePageComponent {
+  constructor() { }
+
+}
